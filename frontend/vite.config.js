3dc6f040efb74bfe1c915af25fa345042df90102
Md Sajid Ansari <129252454+Sajid788@users.noreply.github.com>
2025-06-26 23:42:50 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'https://revenue-llp.vercel.app',
+        target: 'https://tes-g9jw.vercel.app',
         changeOrigin: true,
         secure: false
       }
